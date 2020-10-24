f271a60798703575cb2e7c5d8dff2f073313b5da
Wentao Yang <wentaoyangquiz@gmail.com>
2020-10-24 12:52:48 -0500

    Added Bootstrap to React

--- codename/src/App.js
+++ codename/src/App.js
@@ -1,25 +1,41 @@
+import { Spinner } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
-      </header>
-    </div>
-  );
+  var b = true;
+  if (b) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hello World
+          </a>
+        </header>
+      </div>
+    );
+  } else { // Loading screen
+    const loadStyle = { // Spinner style
+      'position': 'fixed',
+      'top': '50%',
+      'left': '50%',
+    };
+
+    return ( // Return loading screen
+      <div style={loadStyle}>
+        <Spinner animation="border" variant="dark" />
+      </div>
+    );
+  }
 }
 
 export default App;
