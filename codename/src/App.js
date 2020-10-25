088a1eceade8e3ed1515d43e48860c166d4dd869
vivazavala1 <kennethz180@yahoo.com>
2020-10-24 21:00:02 -0500

    More features were added

--- codename/src/App.js
+++ codename/src/App.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-import React from 'react';
-import {
-  Spinner,
-} from 'react-bootstrap';
-import MapChart from './components/MapChart';
-import Header from './components/Header';
-import Footer from './components/Footer';
+
+import React, { useState } from 'react';
 import Home from './Views/Home';
-=======
 import Header from './Components/Header';
 import Context from './Components/Context';
 import MapChart from './Components/MapChart';
@@ -15,12 +8,11 @@ import Analysis from './Components/Analysis';
 import Methodology from './Components/Methodology';
 import About from './Components/About';
 import Footer from './Components/Footer';
->>>>>>> 131db0d5ae30043649f2ba54cdb9495c1ef8cdb7
+
 import {
   BrowserRouter as Router,
 } from 'react-router-dom';
 import DatePicker from 'react-date-picker'
-import React, { useState } from 'react';
 import './App.css';
 
 function App() {
