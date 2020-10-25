1b98afcb5ffff66d67b64bae2eccd6b8526e4fa4
Wentao Yang <wentaoyangquiz@gmail.com>
2020-10-24 20:13:06 -0500

    Added date selection

--- codename/src/App.js
+++ codename/src/App.js
@@ -8,15 +8,26 @@ import Footer from './Components/Footer';
 import {
   BrowserRouter as Router,
 } from 'react-router-dom';
+import DatePicker from 'react-date-picker'
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [dateValue, onChange] = useState(new Date());
+
   return (
     <div className="App">
       <Router>
         <Header/>
         
         <Context/>
+
+        <p>Enter a date between 1/22/20 and 11/22/20 for the map{"\n"}</p>
+        <DatePicker
+          onChange={onChange}
+          value={dateValue}
+        />
+
         <MapChart/>
         <Analysis/>
         <Methodology/>
